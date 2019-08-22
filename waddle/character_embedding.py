4e1eac95d6e5c41e2c99970a9cff457f0a6c3363
Andrew Maher <andrewtmmaher@gmail.com>
2019-08-22 09:58:44 +0100

    Make main run function parameterisable

--- waddle/character_embedding.py
+++ waddle/character_embedding.py
@@ -13,24 +13,27 @@ WINDOW_SIZE = 2
 NUMBER_EVALUATION_NEIGHBOURS = 5
 NUMBER_TRAINING_STEPS = 10000
 
+EVALUATION_CHARACTERS = ['a', 'A', 'R', 'r']
 
-def run(raw_whatsapp_chat):
+
+def run(raw_whatsapp_chat, embedding_dimension, window_size,
+        number_training_steps, number_evaluation_neighbours):
 
     whatsapp_chat = clean(raw_whatsapp_chat)
 
     text = Text(whatsapp_chat)
 
-    model, validation_model = build_embedding_models(text.vocabulary_size, EMBEDDING_DIMENSION)
+    model, validation_model = build_embedding_models(text.vocabulary_size, embedding_dimension)
 
-    validation_examples = [text.character_to_index[c] for c in ['a', 'A', 'R', 'r']]
+    validation_examples = [text.character_to_index[c] for c in EVALUATION_CHARACTERS]
 
     similarity_callback = SimilarityCallback(
-        NUMBER_EVALUATION_NEIGHBOURS, validation_examples, text, validation_model)
+        number_evaluation_neighbours, validation_examples, text, validation_model)
 
-    context_data = ContextData.from_text(text, WINDOW_SIZE)
+    context_data = ContextData.from_text(text, window_size)
 
     train_embedding_model(
-        model, similarity_callback, context_data, NUMBER_EVALUATION_NEIGHBOURS)
+        model, similarity_callback, context_data, number_training_steps)
 
 
 if __name__ == '__main__':
@@ -48,5 +51,14 @@ if __name__ == '__main__':
     print('Loading chat from {}'.format(args.whatsap_chat_path))
     chat_data = load_chat_from_path(args.whatsap_chat_path)
 
+    print('Number of training steps: {}'.format(args.number_training_steps))
+    print('Embedding dimensionality: {}'.format(args.embedding_dimension))
+
     print('Chat contains {} different messages'.format(len(chat_data)))
-    run(chat_data)
+    run(
+        chat_data,
+        args.embedding_dimension or EMBEDDING_DIMENSION,
+        WINDOW_SIZE,
+        args.number_training_steps or NUMBER_TRAINING_STEPS,
+        NUMBER_EVALUATION_NEIGHBOURS
+    )
